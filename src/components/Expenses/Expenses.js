c2a7649d41d316c985c9de9fb595082ce627d205
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-06 09:13:43 +0200

    Verified by Marti4n v1.1.8 outputing conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,17 +9,28 @@ const Expenses = (props) => {
   const classes = myStyle()
   const [filteredYear, setFilteredYear]= useState("2021")
   
-  const filterChangeHandler = selectedYear =>{
+  const filterChangeHandler = (selectedYear) =>{
     setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+
+  let expensesContent = <p>Couldn't find any Expenses!</p>
+  if(filteredExpenses.length > 0) {
+    expensesContent =   filteredExpenses.map(expense => <ExpenseItem 
+      key    = {expense.id}
+      title  = {expense.title}  
+      amount = {expense.amount}
+      date   = {expense.date} 
+      /> )
   }
   return (
     <Card className={classes.expenses}>
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-    {props.items.map(expense => <ExpenseItem 
-    title  = {expense.title}  
-    amount = {expense.amount}
-    date   = {expense.date} 
-    /> )}
+    {expensesContent}
     </Card>
   );
 }
